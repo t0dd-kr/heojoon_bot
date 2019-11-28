b89318360ee788596905dcb8413d7d2587628d93
t0dd <swjasontodd@gmail.com>
2019-11-29 03:15:39 +0900

    모바일 디버깅

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -46,7 +46,7 @@
       </div>
       <div class="container-input">
         <div class="text-input">
-          <input type="text" name="" v-model="userChat" :placeholder="placeholder" @keydown="($event) => { if ($event.keyCode === 13) { sendChat() } }">
+          <input type="text" name="" v-model="userChat" :placeholder="placeholder" @keydown="($event) => { if ($event.keyCode === 13) { sendChat() } }" @focus="alert">
           <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="535.5px" height="535.5px" viewBox="0 0 535.5 535.5" style="enable-background:new 0 0 535.5 535.5;" xml:space="preserve" @click="sendChat">
           	<!-- <g id="send">
           		<polygon points="0,497.25 535.5,267.75 0,38.25 0,216.75 382.5,267.75 0,318.75"/>
@@ -121,6 +121,9 @@ export default {
     })
   },
   methods: {
+    alert: function () {
+      alert('input focused')
+    },
     clickOption: function (option) {
       this.optionList = []
       this.chats.push({
